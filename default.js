fb802fbf1641e8640e321d017d2bcc00eb646521
zuoqiuliang <zuoqiuliang@huatu.com>
2026-02-09 11:43:07 +0800

    feat:增加 default文件

---
+++ default.js
@@ -0,0 +1,11 @@
+module.exports = {
+	port: 3009,
+	database: {
+		DATABASE: "myboke",
+		USER: "root",
+		PASSWORD: "ZUO+18232249132",
+		HOST: "localhost",
+		PORT: "3306"
+	},
+	jwt_secret: "zql"
+};
